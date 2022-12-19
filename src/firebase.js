f32105eb2c8afc8cb00b824c8d01930584284904
jenatayeva <atayevajennet1126@gmail.com>
2022-12-19 20:12:47 +0500

    disneyClone

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD4Xyns7UrdgW2Xdyr3M1Zw2u5e4QDhCII",
-  authDomain: "myreactappexersice.firebaseapp.com",
-  projectId: "myreactappexersice",
-  storageBucket: "myreactappexersice.appspot.com",
-  messagingSenderId: "434549029463",
-  appId: "1:434549029463:web:f0c043682c55d81b018112"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
